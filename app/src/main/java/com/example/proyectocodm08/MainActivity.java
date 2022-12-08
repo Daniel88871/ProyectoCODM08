83b2d25c1334e9c3abf47774fe1d228fef89f06f
Daniel88871 <druizl01@elpuig.xeill.net>
2022-12-08 15:58:52 +0100

    waw

--- app/src/main/java/com/example/proyectocodm08/MainActivity.java
+++ app/src/main/java/com/example/proyectocodm08/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.proyectocodm08;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.proyectocodm08.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                // Top-level destinations:
+                R.id.homeFragment2, R.id.armasFragment, R.id.ventajasFragment, R.id.mejorasFragment, R.id.m4Fragment, R.id.kastovFragment, R.id.silenciomortalFragment, R.id.carroneroFragment, R.id.escudoFragment, R.id.municionFragment
+        )
+                .setOpenableLayout(binding.drawerLayout)
+                .build();
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
     }
 }
